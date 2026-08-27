77e33262df2fc52496edafabef169c9fa9338c2f
jishnu326 <jintorr55@gmail.com>
2026-08-27 12:19:01 +0530

    Change onload to DOMContentLoaded event listener

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-onload = () => {
-  const c = setTimeout(() => {
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
     document.body.classList.remove("not-loaded");
-    clearTimeout(c);
   }, 1000);
-};
+});
